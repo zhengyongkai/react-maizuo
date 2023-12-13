c4d9eab66936739a02bd90bb0989ac73e4e56063
zhengyongkai <825947557@qq.com>
2023-12-13 20:27:03 +0800

    🐞 fix(vite.config.ts):
    1. 修复了打包出现React找不到的问题

--- vite.config.ts
+++ vite.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          react: 'React',
-          'react-dom': 'react-dom',
+          // react: 'React',
+          // 'react-dom': 'react-dom',
         },
       },
     },
