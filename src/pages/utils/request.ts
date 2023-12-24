1e7ca2bb5c7b859b7d345e05f0c969f3039fed8f
zhengyongkai <825947557@qq.com>
2023-12-24 21:48:12 +0800

    ✨ feat(page):
    1. 当出现401的时候删除个人数据

--- src/pages/utils/request.ts
+++ src/pages/utils/request.ts
@@ -5,17 +5,12 @@
  */
 import { Toast } from 'antd-mobile';
 import axios from 'axios';
+import store from '@/store';
 import { clearUserData } from '@/store/common/user';
 
 // import { createHashHistory } from "history";
 import cookie from './cookie';
-
-// const customHistory = createHashHistory();
-
-// import { Route } from "react-router-dom";
-
 axios.interceptors.request.use((requestConfig) => {
-  // console.log(requestConfig);
   requestConfig.headers['X-Client-Info'] =
     requestConfig.headers['X-Client-Info'] ??
     '{"a":"3000","ch":"1002","v":"5.2.1","e":"16986321061049067236884481","bc":"110100"}';
@@ -24,10 +19,6 @@ axios.interceptors.request.use((requestConfig) => {
   requestConfig.headers['X-Requested-With'] =
     requestConfig.headers['X-Requested-With'] ?? 'XMLHttpRequest';
   requestConfig.headers['X-Token'] = cookie.getCookie('x-Token');
-
-  // requestConfig.headers["Content-Type"] = "application/x-www-form-urlencode";
-  // console.log(requestConfig.data);
-  // console.log(requestConfig);
   return requestConfig;
 });
 
@@ -38,6 +29,7 @@ axios.interceptors.response.use((response) => {
       Toast.show(response.data.msg);
       return Promise.reject(response.data.msg);
     case 401:
+      store.dispatch(clearUserData());
       return Promise.reject(response.data.msg);
   }
   return response.data;
